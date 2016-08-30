d464ef19a5ec16a9a685ef68a9b047f422799f76
Nick Williams <nick22891@yahoo.com>
2016-08-30 14:25:25 -0500

    Test mongoose models set up with working GET and POST routes.

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+
+var model = require('./models/db');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
