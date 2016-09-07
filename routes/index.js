ec9cf13ddbe04f1ab6b71935b39122b2e1890c24
Nick Williams <nick22891@yahoo.com>
2016-09-07 14:53:26 -0500

    Implementing the designed pages as views - I still need to set up Angular on these.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Harvest' });
+});
+
+router.get('/signin', function(req, res, next) {
+  res.render('signin', { title: 'Harvest' });
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Harvest' });
+});
+
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', { title: 'Harvest' });
+});
+
+router.get('/confirmation', function(req, res, next) {
+  res.render('confirmation', { title: 'Harvest' });
+});
+
+router.get('/validated', function(req, res, next) {
+  res.render('validated', { title: 'Harvest' });
 });
 
 module.exports = router;
