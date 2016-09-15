7e600823c9736db4d0d192f8f695dfb7a3dfa372
Nick Williams <nick22891@yahoo.com>
2016-09-15 17:32:57 -0500

    Angular routing for website views for signup, signin, dashboard, user flow. Re-uses partials where possible and switches out content as necessary.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -2,4 +2,13 @@
  * Created by matjames007 on 9/10/16.
  */
 
-angular.module('harvestv2', []);
+angular.module('harvestv2', ["ngRoute"]).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {templateUrl: '../partials/home.html', controller: 'HomepageCtrl'});
+    $routeProvider.when('/signin', {templateUrl: '../partials/signin.html', controller: 'HomepageCtrl'});
+    $routeProvider.when('/signup', {templateUrl: '../partials/signup.html', controller: 'HomepageCtrl'});
+    $routeProvider.when('/confirmation', {templateUrl: '../partials/confirmation.html', controller: 'HomepageCtrl'});
+    $routeProvider.when('/dashboard', {templateUrl: '../partials/dashboard.html', controller: 'HomepageCtrl'});
+    $routeProvider.otherwise({redirectTo: '/'});
+
+    $locationProvider.html5Mode(true);
+}]);
