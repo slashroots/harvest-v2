245eb1a18d48d7c185ea572e008667dc176a4587
thinkking <lee.christopher.murray@gmail.com>
2016-09-24 13:58:39 -0500

    both swagger and redoc are up and running fully

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/docs', function(req, res, next) {
-  res.render('docs', { title: 'HarvestAPI Specs' });
-});
-
 module.exports = router;
