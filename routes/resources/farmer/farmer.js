a92c6366b06fbd860aebed056a581a2ff242ce08
Nick Williams <nick22891@yahoo.com>
2016-11-14 08:17:29 -0500

    The farmers resource can now be accessed properly with no errors using the Sequelize library. More importantly, we can now specify a limit and offset parameter to effectively paginate the data and we can also search by specific fields.

--- routes/resources/farmer/farmer.js
+++ routes/resources/farmer/farmer.js
@@ -13,88 +13,133 @@ var Sequelize = require('sequelize');
  * @param res
  * @param next
  */
-exports.getAllFarmers = function(req, res, next) {
 
+var sequelize = new Sequelize('mssql://' + process.env.MSSQL_USER + ':' + process.env.MSSQL_PASS + '@' + process.env.MSSQL_SERVER + ':1433/' + process.env.MSSQL_DB);
+
+var Farmer = sequelize.define('std_reg_farmer_profile_table', {
+    IDX_Farmer_Profile: {
+        primaryKey: true,
+        type: Sequelize.STRING,
+        field: 'IDX_Farmer_Profile'
+    },
+    IDX_Stakeholder: {
+        type: Sequelize.STRING,
+        field: 'IDX_Stakeholder'
+    },
+    Education_Level: {
+        type: Sequelize.STRING,
+        field: 'Education_Level'
+    },
+    Farmer_Type: {
+        type: Sequelize.STRING,
+        field: 'Farmer_Type'
+    },
+    Main_Agri_Activity: {
+        type: Sequelize.STRING,
+        field: 'Main_Agri_Activity'
+    },
+    Holding_Start: {
+        type: Sequelize.STRING,
+        field: 'Holding_Start'
+    },
+    Nearest_Police_Station: {
+        type: Sequelize.STRING,
+        field: 'Nearest_Police_Station'
+    },
+    Jas_Farmer_Group: {
+        type: Sequelize.STRING,
+        field: 'Jas_Farmer_Group'
+    },
+    Farmer_Profile_Remarks: {
+        type: Sequelize.STRING,
+        field: 'Farmer_Profile_Remarks'
+    },
+    Create_Date: {
+        type: Sequelize.STRING,
+        field: 'Create_Date'
+    },
+    Update_Date: {
+        type: Sequelize.STRING,
+        field: 'Update_Date'
+    },
+    Respondent: {
+        type: Sequelize.STRING,
+        field: 'Respondent'
+    },
+    Manager: {
+        type: Sequelize.STRING,
+        field: 'Manager'
+    },
+    Income_Source: {
+        type: Sequelize.STRING,
+        field: 'Income_Source'
+    },
+    Occupation: {
+        type: Sequelize.STRING,
+        field: 'Occupation'
+    },
+    Training_Method: {
+        type: Sequelize.STRING,
+        field: 'Training_Method'
+    },
+    Agri_Institution: {
+        type: Sequelize.STRING,
+        field: 'Agri_Institution'
+    },
+    Qualification: {
+        type: Sequelize.STRING,
+        field: 'Qualification'
+    },
+    Agri_Training: {
+        type: Sequelize.STRING,
+        field: 'Agri_Training'
+    },
+    Live_On_Farm: {
+        type: Sequelize.STRING,
+        field: 'Live_On_Farm'
+    }
+}, {
+    timestamps: false,
+    freezeTableName: true // Model tableName will be the same as the model name
+});
+
+
+exports.getAllFarmers = function(req, res, next) {
     var fakeblock = new Fakeblock({
         acl: farmersAcl,
         userRole: req.user.ap_app_role.ro_role_name
     });
 
-    var sequelize = new Sequelize('mssql://' + process.env.MSSQL_USER + ':' + process.env.MSSQL_PASS + '@' + process.env.MSSQL_SERVER + ':1433/' + process.env.MSSQL_DB);
+    /*
+    Here, we retrieve the offset and limit parameters from the query if they exist and then remove them (as well as
+    the access_token in case the user opted to specify it in the url directly) so that the remaining fields can be used
+    for searching.
+     */
+    var limit = req.query.limit || 100;
+    var offset = req.query.offset || 0;
+    delete req.query.access_token;
+    delete req.query.limit;
+    delete req.query.offset;
 
-    var Farmer = sequelize.define('std_reg_farmer_profile_table', {
-        IDX_Farmer_Profile: {
-            primaryKey: true,
-            type: Sequelize.STRING,
-            field: 'IDX_Farmer_Profile' // Will result in an attribute that is firstName when user facing but first_name in the database
-        },
-        IDX_Stakeholder: {
-            type: Sequelize.STRING,
-            field: 'IDX_Stakeholder'
-        },
-        Education_Level: {
-            type: Sequelize.STRING,
-            field: 'Education_Level'
-        },
-        Farmer_Type: {
-            type: Sequelize.STRING,
-            field: 'Farmer_Type'
-        },
-        Main_Agri_Activity: {
-            type: Sequelize.STRING,
-            field: 'Main_Agri_Activity'
-        }
-    }, {
-        timestamps: false,
-        freezeTableName: true // Model tableName will be the same as the model name
-    });
-
-    Farmer.find().then(function(err, farmers) {
-        if(err) {
-            next(err);
-        } else {
+    Farmer.findAll({
+        where: req.query,
+        offset: parseInt(offset),
+        limit: parseInt(limit)
+    }).then(function(farmers) {
+            for (var i = 0;i<farmers.length;i++) farmers[i] = fakeblock.applyAcl(farmers[i], 'get');
             res.send(farmers);
-            //res.send(fakeblock.applyAcl(farmers, 'get'));
-        }
-    });
 
+    });
 };
 
 exports.getFarmerByID = function(req, res, next) {
-
-    // a fakeblock instance created for each user and each ACL
     var fakeblock = new Fakeblock({
         acl: farmersAcl,
         userRole: req.user.ap_app_role.ro_role_name
     });
 
-    var connection1 = new sql.Connection(Common.getResourceDBConfig(), function(err) {
-        if(err) {
-            return next(err);
-        }
-
-        // Query
-        var request = new sql.Request(connection1); // or: var request = connection1.request();
-        request.query('select * from std_reg_farmer_profile_table where std_reg_farmer_profile_table.IDX_StakeHolder = ' + req.params.id, function(err, recordset) {
-            // ... error checks
-            if(err) {
-                return next(err);
-            } else {
-
-                for (i = 0; i < recordset.length; i++) {
-                    recordset[i] = fakeblock.applyAcl(recordset[i], 'get');
-                }
-                res.send(recordset);
-            }
-        });
+    Farmer.findOne({ where: {IDX_Stakeholder: req.params.id} }).then(function(farmers) {
+        res.send(fakeblock.applyAcl(farmers, 'get'));
 
     });
-
-    connection1.on('error', function(err) {
-        if(err) {
-            return next(err);
-        }
-    });
-
-
 };
